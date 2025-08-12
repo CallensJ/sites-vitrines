31d10c2427bf7f015b2a4fa0ecf2cb8632bb136e
CallensJ <callens.johan@protonmail.com>
2025-08-12 10:21:01 +0200

    added responsive navbar

---
+++ soficraft/script.js
@@ -0,0 +1,6 @@
+const toggle = document.querySelector(".menu-toggle");
+const navbar = document.querySelector(".navbar");
+
+toggle.addEventListener("click", () => {
+  navbar.classList.toggle("active");
+});
