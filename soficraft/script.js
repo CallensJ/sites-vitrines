3ee7adceb61006168c8626590033b89f85ef63d6
CallensJ <callens.johan@protonmail.com>
2025-08-14 12:08:07 +0200

    added images gallerie

--- soficraft/script.js
+++ soficraft/script.js
@@ -4,3 +4,26 @@ const navbar = document.querySelector(".navbar");
 toggle.addEventListener("click", () => {
   navbar.classList.toggle("active");
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const galleryContainer = document.querySelector(".gallerie");
+
+  fetch("./gallery.json")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Erreur lors du chargement du JSON");
+      }
+      return response.json();
+    })
+    .then((images) => {
+      images.forEach(({ src, alt }) => {
+        const img = document.createElement("img");
+        img.src = src;
+        img.alt = alt;
+        galleryContainer.appendChild(img);
+      });
+    })
+    .catch((error) => {
+      console.error("Impossible de charger la galerie :", error);
+    });
+});
